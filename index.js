f8bb5357c04b87e526410020df1b43e5559de9fd
Jannis Gundermann <jannis@gundermann.me>
2016-11-24 15:35:23 +1300

    Fixes the handlebar helper's data hash not being passed to the Faker function call.

--- index.js
+++ index.js
@@ -10,10 +10,20 @@ module.exports = function handlebarsFakerHelper (fakeName /*, [...], options */)
 
   var fakeFunction = faker[fakeNameParts[0]][fakeNameParts[1]];
   var options = arguments[arguments.length - 1];
+  var fakerOptions = options.hash;
   var fakerArguments = Array.prototype.slice.call(arguments, 1, arguments.length - 1);
+
+  if (fakerOptions) {
+    // pushes the handlebars hash into the argument list of the faker call.
+    // this ensures faker methods that depend on option objects like Faker.random.number({min, max, precision})
+    // can be properly given their options object.
+    fakerArguments.push(fakerOptions);
+  }
+
   var language = options.hash.lang || defaultLanguage;
   var fakerLanguage = faker.locale;
   faker.locale = language;
+
   var result = fakeFunction.apply(faker, fakerArguments);
   faker.locale = fakerLanguage;
   return result;
